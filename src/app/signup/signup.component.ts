e14ee0d27dedb279cea194b36f984d400f36feea
Ravneek Bhullar <77533634+Ravneek29@users.noreply.github.com>
2022-07-15 23:11:19 -0400

    update2

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignUpComponent implements OnInit {
+  signupForm!: FormGroup;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.signupForm = this.createFormGroup();
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      name: new FormControl("", [Validators.required, Validators.minLength(2)]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+
+    });
+  }
+
+  signup(): void{
+    console.log(this.signupForm.value);
+  }
+
+}
