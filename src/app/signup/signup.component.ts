3370cd9c6ecdc5ad217e114cd57fc6fac3cd3e2a
Nisarg Patel <nisarg38@gmail.com>
2022-07-18 11:08:31 -0400

    SignUp-Page-Post-Request-Fix

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -28,8 +28,10 @@ export class SignUpComponent implements OnInit {
       useremail: new FormControl("", [Validators.required, Validators.email]),
       userpswd: new FormControl("", [
         Validators.required,
-        Validators.minLength(7),
+        Validators.minLength(7)
       ]),
+      userphone: new FormControl("", [Validators.required, Validators.minLength(2)]),
+      useradd: new FormControl("", [Validators.required, Validators.minLength(2)]),
     });
   }
 
@@ -52,12 +54,10 @@ export class SignUpComponent implements OnInit {
       
     // Sending POST request through Admin Service 
     if(this.f.username.valid &&  this.f.useremail.valid &&  this.f.userpswd.valid &&  this.f.userphone.valid &&  this.f.useradd.valid )
-
+{
       this.adminservice.createUser(this.f.username.value, this.f.useremail.value, this.f.userpswd.value,  this.f.userphone.value,  this.f.useradd.value)
       .subscribe({
         next: () => {
-          this.dialogRef.close();
-          
           this.Toast.fire({
             icon: 'success',
             title: ' Account created !!'
@@ -65,7 +65,7 @@ export class SignUpComponent implements OnInit {
       
         }
     });
-
+  }
       this.Toast.fire({
         icon: 'error',
         title: 'UnSuccessful :('
