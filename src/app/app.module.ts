d40a52d4f8de4d26e578d5c133f745dc4703c2bc
sammy2byou <sammy2bpizza@gmail.com>
2022-07-30 21:38:26 -0400

    Changing how I'm doing this
    Deleting product card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { CategoryComponent } from './category/category.component';
-import { ProductCardComponent } from './product-card/product-card.component';
+
 
 @NgModule({
   declarations: [
@@ -50,8 +50,7 @@ import { ProductCardComponent } from './product-card/product-card.component';
     LoginComponent,
     SignUpComponent,
     ProductPageComponent,
-    CategoryComponent,
-    ProductCardComponent
+    CategoryComponent
   ],
   imports: [
     MatFormFieldModule,
