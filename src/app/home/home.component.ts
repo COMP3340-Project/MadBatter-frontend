4c3e634f4b133f83a1549066877489ad5f36b367
shawnmuscat <91565449+shawnmuscat@users.noreply.github.com>
2022-08-09 22:09:10 -0400

    Added weather

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,13 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  WeatherData:any;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     slideshow();
+    this.getWeatherData();
+    console.log(this.WeatherData);
+  }
+  routecat(catid:number, catname:string)
+  {
+    console.log(catid);
+    const test = "?id:"
+    this.router.navigateByUrl(`/category?id=${catid}&catname=${catname}`);
   }
 
+  getWeatherData()
+  {
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=42.3&lon=-83.0364&appid=49e29eea7059655c43771c486c95508e&units=metric')
+        .then(response=>response.json())
+        .then(data=>{this.setWeatherData((data))})
+  }
+
+  setWeatherData(data: any)
+  {
+    this.WeatherData = data;
+  }
 }
+
+
 
 let myIndex = 0;
 //setTimeout(slideshow, 0);
@@ -33,4 +55,5 @@ function slideshow()
   x[myIndex - 1].style.display = "block";
   setTimeout(slideshow, 2000);
 }
+
 
